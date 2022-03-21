5d7852461a18e7624f828ae6cdb594770db74977
gopisree <lingamsettygopisree@gmail.com>
2022-03-21 14:11:44 +0530

    web server program

---
+++ app.py
@@ -0,0 +1,11 @@
+from mimetypes import init
+from flask import Flask 
+
+app=Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return('hello GPCET-AI')
+
+if (__name__=="__main__"):
+    app.run()
